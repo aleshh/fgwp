e75f16e6f47b05a041286c8de03c5475c3ae313f
Alesh Houdek <mail@alesh.com>
2016-02-25 06:08:13 -0500

    wp outputing category list with links

--- header.php
+++ header.php
@@ -12,25 +12,64 @@
 
 <div id="header">
   <div id="menu">
-      <div class="column">
+    <div class="column">
       <a id="home" href="/" ><img alt="Felice Grodin" src="/resources/felicegrodin2.gif" /></a>
-        </div>
-        <div class="column">
-          <a href="/drawings-figural" class="active" >DRAWINGS::figural</a><a href="/drawings-expansion">DRAWINGS::expansion</a><a href="/drawings-biological">DRAWINGS::biological</a>
-        </div>
-        <div class="column3" >
-          <a href="/drawings-topo-field">DRAWINGS::topo/field</a><a href="/drawings-laminar">DRAWINGS::laminar</a>
-        </div>
-        <div class="column" >
-          <a href="/installation">INSTALLATION</a><a href="/sculpture">SCULPTURE</a><a href="/assemblage">ASSEMBLAGE</a>
-        </div>
-        <div class="column">
-          <a href="/video-digital">VIDEO/DIGITAL</a><a href="/text">TEXT</a><a href="/info">INFO</a>
-        </div>
-        <div class="lastcolumn">
-          &nbsp;
-        </div>
-  </div>
-</div>
+    </div>
+
+
+    <div class="column">
+    <?php
+      $cat = get_categories( array(
+        'hide_empty'   => 0,
+        'orderby'     => 'name'
+        ));
+      $i = 0;
+      foreach ($cat as $category) {
+        $i ++;
+        $link = get_category_link($category->term_id);
+        echo "<a href='$link'>$category->name</a>";
+        if ($i % 3 == 0) echo "</div><div class='column'>";
+      }
+
+// foreach( $categories as $category ) {
+//     $category_link = sprintf( '<a href="%1$s" alt="%2$s">%3$s</a>'
+//         esc_url( get_category_link( $category->term_id ) ),
+//         esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
+//         esc_html( $category->name )
+//     );
+
+//     echo '<p>' . sprintf( esc_html__( 'Category: %s', 'textdomain' ), $category_link ) . '</p> ';
+//     echo '<p>' . sprintf( esc_html__( 'Description: %s', 'textdomain' ), $category->description ) . '</p>';
+//     echo '<p>' . sprintf( esc_html__( 'Post Count: %s', 'textdomain' ), $category->count ) . '</p>';
+// }
+    ?>
+
+    </div> <!-- .column -->
+
+<!--
+    <div class="column">
+      <a href="/drawings-figural" class="active" >DRAWINGS::figural</a>
+      <a href="/drawings-expansion">DRAWINGS::expansion</a>
+      <a href="/drawings-biological">DRAWINGS::biological</a>
+    </div>
+    <div class="column3" >
+      <a href="/drawings-topo-field">DRAWINGS::topo/field</a>
+      <a href="/drawings-laminar">DRAWINGS::laminar</a>
+    </div>
+    <div class="column" >
+      <a href="/installation">INSTALLATION</a>
+      <a href="/sculpture">SCULPTURE</a>
+      <a href="/assemblage">ASSEMBLAGE</a>
+    </div>
+    <div class="column">
+      <a href="/video-digital">VIDEO/DIGITAL</a><a href="/text">TEXT</a><a href="/info">INFO</a>
+    </div>
+    <div class="lastcolumn">
+      &nbsp;
+    </div> -->
+
+
+  </div> <!-- #menu -->
+</div> <!-- #header -->
 
 <div id="content">
