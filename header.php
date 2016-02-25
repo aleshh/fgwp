e7ba267547e3b4617ae044670c37a6d3c4f5f32d
Alesh Houdek <mail@alesh.com>
2016-02-25 06:33:33 -0500

    category list formatting improvements

--- header.php
+++ header.php
@@ -28,20 +28,18 @@
         $i ++;
         $link = get_category_link($category->term_id);
         echo "<a href='$link'>$category->name</a>";
-        if ($i % 3 == 0) echo "</div><div class='column'>";
+        if ($i % 3 == 0) {
+          if ($i == 3) {
+            // column 3 is between two sets of drawings so no line
+            echo "</div><div class='col-break'>";
+          } else {
+            echo "</div><div class='column'>";
+          }
+        }
+
+
       }
 
-// foreach( $categories as $category ) {
-//     $category_link = sprintf( '<a href="%1$s" alt="%2$s">%3$s</a>'
-//         esc_url( get_category_link( $category->term_id ) ),
-//         esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
-//         esc_html( $category->name )
-//     );
-
-//     echo '<p>' . sprintf( esc_html__( 'Category: %s', 'textdomain' ), $category_link ) . '</p> ';
-//     echo '<p>' . sprintf( esc_html__( 'Description: %s', 'textdomain' ), $category->description ) . '</p>';
-//     echo '<p>' . sprintf( esc_html__( 'Post Count: %s', 'textdomain' ), $category->count ) . '</p>';
-// }
     ?>
 
     </div> <!-- .column -->
