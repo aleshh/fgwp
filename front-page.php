40e541c1f44932f34a76710a8bb2329da0d352d3
Alesh Houdek <mail@alesh.com>
2016-04-05 06:46:07 -0400

    made main image on homepage clickable

--- front-page.php
+++ front-page.php
@@ -1,28 +1,30 @@
 <?php get_header(); ?>
 
 <!-- <div id="caption" style="background-color:#444;" >&nbsp;</div> -->
+<a href="/drawings-figural/">
+  <div id="full-page" style="
+    <?php
+      $filecount = 0;
+      $fileexist = true;
+      do {
+        if (file_exists("resources/homepage-images/".($filecount+1).".jpg")) {
+          $filecount++;
+        } else {
+          $fileexist = false;
+        }
+      } while ($fileexist == true);
 
-<div id="full-page" style="
-  <?php
-    $filecount = 0;
-    $fileexist = true;
-    do {
-      if (file_exists("resources/homepage-images/".($filecount+1).".jpg")) {
-        $filecount++;
+      if ($filecount == 0) {
+        echo "Error: no images found!";
       } else {
-        $fileexist = false;
+        echo "background-image:url(resources/homepage-images/".(rand(1,$filecount)).".jpg\")";
+        // echo "<img src=\"resources/homepage-images/".(rand(1,$filecount)).".jpg\" alt=\"Felice Grodin, detail\" />";
       }
-    } while ($fileexist == true);
+    ?>
 
-    if ($filecount == 0) {
-      echo "Error: no images found!";
-    } else {
-      echo "background-image:url(resources/homepage-images/".(rand(1,$filecount)).".jpg\")";
-      // echo "<img src=\"resources/homepage-images/".(rand(1,$filecount)).".jpg\" alt=\"Felice Grodin, detail\" />";
-    }
-  ?>
+    ">
+  </div>
+</a>
 
-  ">
-</div>
 
 <?php get_footer(); ?>
